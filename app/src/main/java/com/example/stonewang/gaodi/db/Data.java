5bd3582bda1bd4ef8669021aad0a2dd111810c6b
stoneWangL <stoneWang_L@163.com>
2017-02-21 21:41:19 +0800

    加入创建数据库和表的各项配置

---
+++ app/src/main/java/com/example/stonewang/gaodi/db/Data.java
@@ -0,0 +1,17 @@
+package com.example.stonewang.gaodi.db;
+
+/**
+ * Created by stoneWang on 2017/2/21.
+ */
+
+public class Data {
+    public String uniquekey;
+    public String title;
+    public String date;
+    public String category;
+    public String author_name;
+    public String url;
+    public String thumbnail_pic_s;
+    public String thumbnail_pic_s02;
+    public String thumbnail_pic_s03;
+}
---
+++ app/src/main/java/com/example/stonewang/gaodi/db/JsonBean.java
@@ -0,0 +1,47 @@
+package com.example.stonewang.gaodi.db;
+
+import com.example.stonewang.gaodi.db.Data;
+
+import java.util.List;
+
+/**
+ * Created by stoneWang on 2017/2/21.
+ */
+
+public class JsonBean {
+
+    public String reason;
+    public Result result;
+    public int error_code;
+
+    public static class Result{
+
+        public String stat;
+
+        public List<Data> data;
+    }
+
+    public String getReason() {
+        return reason;
+    }
+
+    public void setReason(String reason) {
+        this.reason = reason;
+    }
+
+    public Result getResult() {
+        return result;
+    }
+
+    public void setResult(Result result) {
+        this.result = result;
+    }
+
+    public int getError_code() {
+        return error_code;
+    }
+
+    public void setError_code(int error_code) {
+        this.error_code = error_code;
+    }
+}
