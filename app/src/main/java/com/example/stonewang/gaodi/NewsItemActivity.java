3e591fed55deadd29c89f86165e2faa19c56120a
stoneWangL <stoneWang_L@163.com>
2017-04-07 00:25:01 +0800

    真正解决了刷新的问题

--- app/src/main/java/com/example/stonewang/gaodi/NewsItemActivity.java
+++ app/src/main/java/com/example/stonewang/gaodi/NewsItemActivity.java
@@ -6,6 +6,7 @@ import android.support.v4.widget.SwipeRefreshLayout;
 import android.support.v7.app.AppCompatActivity;
 import android.support.v7.widget.LinearLayoutManager;
 import android.support.v7.widget.RecyclerView;
+import android.util.Log;
 import android.view.View;
 import android.widget.Button;
 import android.widget.Toast;
@@ -30,14 +31,11 @@ import okhttp3.Response;
 public class NewsItemActivity extends AppCompatActivity {
     public static NewsItemActivity mactivity;
     private Button backButton;
-    private List<String> dataList = new ArrayList<>();
-    private NewsItemAdapter adapter;
-    /**
-     * News列表
-     */
-    private List<GaoDiNews> gaoDiNewsList;
-
     private SwipeRefreshLayout swipeRefresh;
+    private NewsItemAdapter adapter;
+
+    private List<GaoDiNews> gaoDiNewsList=new ArrayList<>(), All=new ArrayList<>();
+
 
     @Override
     protected void onCreate(@Nullable Bundle savedInstanceState) {
@@ -52,18 +50,34 @@ public class NewsItemActivity extends AppCompatActivity {
                 finish();
             }
         });
-        //向服务器请求
-        sendRequestWithOkHttp();
-        //查询选中的News，去数据库上查询
-        queryNews();
 
+
+        gaoDiNewsList.clear();
+        All.clear();
+        All = DataSupport.findAll(GaoDiNews.class);
+        int i = All.size();String j = ""+i;Log.d("num1", "size="+j);//打印信息
+        if (All.size()>0){
+            //有缓存
+        }else{
+            //向服务器请求
+            sendRequestWithOkHttp();
+            All = DataSupport.findAll(GaoDiNews.class);
+        }
+
+        for (GaoDiNews all:All){
+            gaoDiNewsList.add(all);
+        }
+
+
+
+        //控件初始化，填充内容
         RecyclerView recyclerView = (RecyclerView) findViewById(R.id.recycler_view);
-        LinearLayoutManager layoutManager = new LinearLayoutManager(this);
+        LinearLayoutManager layoutManager = new LinearLayoutManager (this);
         recyclerView.setLayoutManager(layoutManager);
         //添加装饰类
         recyclerView.addItemDecoration(new MyItemDecoration());
-
         adapter = new NewsItemAdapter(gaoDiNewsList);
+
         recyclerView.setAdapter(adapter);
 
         swipeRefresh = (SwipeRefreshLayout) findViewById(R.id.swipe_refresh);
@@ -71,32 +85,40 @@ public class NewsItemActivity extends AppCompatActivity {
         swipeRefresh.setOnRefreshListener(new SwipeRefreshLayout.OnRefreshListener() {
             @Override
             public void onRefresh() {
-                refreshGaoDiNews();
+//                refreshGaoDiNews();
+                DataSupport.deleteAll(GaoDiNews.class);//清空本地数据库缓存
+                sendRequestWithOkHttp();//向服务器发送请求，并插入本地数据库
+                new Thread(new Runnable() {
+                    @Override
+                    public void run() {
+                        try{
+                            Thread.sleep(4000);
+                        }catch (InterruptedException e){
+                            e.printStackTrace();
+                        }
+                        runOnUiThread(new Runnable() {
+                            @Override
+                            public void run() {
+                                gaoDiNewsList.clear();
+                                All.clear();
+                                All = DataSupport.findAll(GaoDiNews.class);
+
+                                int ii = All.size();String jj = ""+ii;Log.d("num2", "size="+jj);//打印信息
+                                for (GaoDiNews all:All){
+                                    gaoDiNewsList.add(all);
+                                }
+
+                                adapter.notifyDataSetChanged();
+                                swipeRefresh.setRefreshing(false);//耗时操作结束
+
+                                Toast.makeText(NewsItemActivity.this, "新闻已更新", Toast.LENGTH_SHORT).show();
+                            }
+                        });
+                    }
+                }).start();
             }
         });
-    }
-    /**
-     * 查询选中的News，去数据库上查询,同时实例化gaoDiNewsList
-     * 如果有缓存，直接解析
-     * 如果没有，再去服务器查询
-     */
-    private void queryNews(){
-        gaoDiNewsList = DataSupport.findAll(GaoDiNews.class);
 
-        if (gaoDiNewsList.size() > 0){
-            dataList.clear();
-            for (GaoDiNews gaoDiNews : gaoDiNewsList){
-                dataList.add(gaoDiNews.getTitle());
-                dataList.add(gaoDiNews.getThumbnail_pic_s());
-                dataList.add(gaoDiNews.getUrl());
-                dataList.add(gaoDiNews.getDate());
-                dataList.add(gaoDiNews.getAuthor_name());
-//                Log.d("Json06", gaoDiNews.getTitle());
-            }
-
-        }else{
-//            Toast.makeText(this, "no find db", Toast.LENGTH_SHORT).show();
-        }
     }
 
     /**
@@ -115,9 +137,9 @@ public class NewsItemActivity extends AppCompatActivity {
                     Response response = client.newCall(request).execute();
                     String responseData = response.body().string();
                     //将服务器返回得到字符串传入处理函数
-//                    Log.d("Json01", responseData);
                     JsonUtil jsonUtil = new JsonUtil();
                     jsonUtil.parseJson(responseData);
+
                 }catch (Exception e){
                     e.printStackTrace();
                 }
@@ -126,34 +148,15 @@ public class NewsItemActivity extends AppCompatActivity {
     }
 
     private void refreshGaoDiNews(){
-        new Thread(new Runnable() {
-            @Override
-            public void run() {
-                try{
-                    Thread.sleep(2000);
-                }catch (InterruptedException e){
-                    e.printStackTrace();
-                }
-                runOnUiThread(new Runnable() {
-                    @Override
-                    public void run() {
-                        View cv = getWindow().getDecorView();//获取当前View
 
-                        DataSupport.deleteAll(GaoDiNews.class);//清空本地数据库缓存
-
-                        sendRequestWithOkHttp();
-                        queryNews();
-
-                        cv.invalidate();//View 重构
-
-
-                        swipeRefresh.setRefreshing(false);//耗时操作结束
-
-                        Toast.makeText(NewsItemActivity.this, "新闻已更新", Toast.LENGTH_SHORT).show();
-                    }
-                });
-            }
-        }).start();
     }
 
+    /**
+     * 刷新界面
+     */
+    private void refresh(){
+        gaoDiNewsList = DataSupport.findAll(GaoDiNews.class);
+        adapter = new NewsItemAdapter(gaoDiNewsList);
+
+    }
 }
--- app/src/main/java/com/example/stonewang/gaodi/adapter/NewsItemAdapter.java
+++ app/src/main/java/com/example/stonewang/gaodi/adapter/NewsItemAdapter.java
@@ -2,22 +2,15 @@ package com.example.stonewang.gaodi.adapter;
 
 import android.content.Context;
 import android.content.Intent;
-import android.content.res.Resources;
-import android.graphics.Bitmap;
-import android.graphics.BitmapFactory;
-import android.support.annotation.NonNull;
-import android.support.annotation.Nullable;
 import android.support.v7.widget.RecyclerView;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
-import android.widget.ArrayAdapter;
 import android.widget.ImageView;
 import android.widget.TextView;
 import android.widget.Toast;
 
 import com.bumptech.glide.Glide;
-import com.example.stonewang.gaodi.MyItemDecoration;
 import com.example.stonewang.gaodi.NewsActivity;
 import com.example.stonewang.gaodi.NewsItemActivity;
 import com.example.stonewang.gaodi.R;
